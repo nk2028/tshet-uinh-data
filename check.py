7e2aa4a497bcb38caea6b803dbae7d706851d74f
sgalal <sgalal.me@outlook.com>
2021-01-05 23:25:10 +0800

    Revert sorting the characters (#1)
    * Revert data to 9849852
    
    * Use 音韻編碼
    
    * Remove 小韻 without 反切
    
    * Update check script

--- check.py
+++ check.py
@@ -12,10 +12,8 @@ with open('小韻表.csv') as f:
 		seen.add((母, 呼, 等, 重紐, 韻, 聲))
 
 with open('字頭表.csv') as f:
-	上一字頭 = '' # 確保字頭有序
 	next(f) # skip header
 	for line in f:
-		字頭, 音韻編碼, 釋義 = line.rstrip('\n').split(',')
+		音韻編碼, 字頭, 釋義 = line.rstrip('\n').split(',')
 		母, 呼, 等, 重紐, 韻, 聲 = decode(音韻編碼)
-		assert 上一字頭 <= 字頭
-		字頭 = 上一字頭
+		assert len(字頭) == 1
