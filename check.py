b20810a2e846a59c5164d30f7e19e77f2f41290c
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-01-17 18:28:34 +0800

    Change data format (#2)

--- check.py
+++ check.py
@@ -1,19 +1,7 @@
-from QieyunEncoder import validate, encode, decode
+from QieyunEncoder import from描述
 
-with open('小韻表.csv') as f:
-	seen = set()
+with open('data.csv') as f:
 	next(f) # skip header
 	for line in f:
-		音韻編碼, 反切 = line.rstrip('\n').split(',')
-		母, 呼, 等, 重紐, 韻, 聲 = decode(音韻編碼)
-		assert len(反切) == 2
-		assert (母, 呼, 等, 重紐, 韻, 聲) not in seen
-		validate(母, 呼, 等, 重紐, 韻, 聲)
-		seen.add((母, 呼, 等, 重紐, 韻, 聲))
-
-with open('字頭表.csv') as f:
-	next(f) # skip header
-	for line in f:
-		音韻編碼, 字頭, 釋義 = line.rstrip('\n').split(',')
-		母, 呼, 等, 重紐, 韻, 聲 = decode(音韻編碼)
-		assert len(字頭) == 1
+		描述, 反切, 字頭, 解釋 = line.rstrip('\n').split(',')
+		from描述(描述)
