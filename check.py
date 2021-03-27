dc1c6ff434f9d278adc68c9e4602a420ff613ef7
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-03-27 18:48:12 +0800

    Change 描述 to 最簡描述

--- check.py
+++ check.py
@@ -1,4 +1,4 @@
-from QieyunEncoder import from描述
+import Qieyun
 import re
 
 pattern = re.compile(r'[\x00-\x7F]')
@@ -12,8 +12,8 @@ def contains_ascii(s: str):
 with open('data.csv') as f:
 	next(f) # skip header
 	for line in f:
-		描述, 反切, 字頭, 解釋 = line.rstrip('\n').split(',')
-		from描述(描述) # function from描述 will perform checks on 描述
+		最簡描述, 反切, 字頭, 解釋 = line.rstrip('\n').split(',')
+		Qieyun.音韻地位.from描述(最簡描述) # function from描述 will perform checks on 描述
 		assert len(反切) in (2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
 		assert len(字頭) == 1, 'The length of 字頭 should be 1'
 		assert not contains_ascii(解釋), '解釋 should not contain any ASCII characters'
