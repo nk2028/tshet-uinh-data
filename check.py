e20a9087c6f015302869a1ef5df4c75c7e3c4b56
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-04-03 09:43:12 +0800

    Add column 韻部原貌

--- check.py
+++ check.py
@@ -12,7 +12,7 @@ def contains_ascii(s: str):
 with open('data.csv') as f:
 	next(f) # skip header
 	for line in f:
-		資料名稱, 小韻號, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip('\n').split(',')
+		資料名稱, 小韻號, 韻部原貌, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip('\n').split(',')
 		Qieyun.音韻地位.from描述(最簡描述) # function from描述 will perform checks on 描述
 		assert len(反切) in (2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
 		assert len(字頭) == 1, 'The length of 字頭 should be 1'
