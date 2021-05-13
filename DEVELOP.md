9675d039f953393e248212354ea764eb3aad8dc3
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-05-13 22:15:02 +0800

    Update file format

---
+++ DEVELOP.md
@@ -0,0 +1,11 @@
+# Develop
+
+The data of rhyme books is originally extracted from [廣韻字音表](https://zhuanlan.zhihu.com/p/20430939), created by poem.
+
+Build
+
+```sh
+git checkout source -- '廣韻(20170209).csv'
+python build.py
+python check.py
+```
--- check.py
+++ check.py
@@ -1,19 +1,21 @@
+from glob import glob
 import QieyunEncoder
 import re
 
 pattern = re.compile(r'[\x00-\x7F]')
 
 def contains_ascii(s: str):
-	'''
-	Check if a string contains at least one ASCII character.
-	'''
-	return bool(pattern.match(s))
+    '''
+    Check if a string contains at least one ASCII character.
+    '''
+    return bool(pattern.match(s))
 
-with open('rhyme_book.csv') as f:
-	next(f) # skip header
-	for line in f:
-		資料名稱, 小韻號, 韻部原貌, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip('\n').split(',')
-		QieyunEncoder.音韻地位.from描述(最簡描述) # function from描述 will perform checks on 描述
-		assert len(反切覈校前) in (2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
-		assert len(字頭覈校前) == 1, 'The length of 字頭 should be 1'
-		assert not contains_ascii(釋義), '釋義 should not contain any ASCII characters'
+for filename in glob('韻書/*.csv'):
+    with open(filename) as f:
+        next(f)  # skip header
+        for line in f:
+            小韻號, 韻部原貌, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip('\n').split(',')
+            QieyunEncoder.音韻地位.from描述(最簡描述) # function from描述 will perform checks on 描述
+            assert len(反切覈校前) in (2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
+            assert len(字頭覈校前) == 1, 'The length of 字頭 should be 1'
+            assert not contains_ascii(釋義), '釋義 should not contain any ASCII characters'
