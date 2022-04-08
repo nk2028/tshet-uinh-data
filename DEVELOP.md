6a34eef0351b928a0eda3a12248a90f0cba2392b
syimyuzya <syimyuzya@gmail.com>
2022-04-09 00:35:37 +0800

    Rebuild 廣韻 v2 from new 音韻地位 data
    Requires Qieyun.js 0.13+

--- DEVELOP.md
+++ DEVELOP.md
@@ -1,11 +1,11 @@
 # Develop
 
-The data of rhyme books is originally extracted from [廣韻字音表](https://zhuanlan.zhihu.com/p/20430939), created by poem.
+The data of 廣韻 is originally extracted from [廣韻字音表](https://zhuanlan.zhihu.com/p/20430939), created by poem.
 
 Build
 
 ```sh
-git checkout source -- '廣韻(20170209).csv'
+git restore -Ws source '廣韻(20170209).csv' 'v2音韻地位.csv'
 python build.py
 python check.py
 ```
--- check.py
+++ check.py
@@ -1,21 +1,32 @@
 from glob import glob
-import QieyunEncoder
 import re
 
-pattern = re.compile(r'[\x00-\x7F]')
+所有母 = '幫滂並明端透定泥來知徹澄孃精清從心邪莊初崇生俟章昌常書船日見溪羣疑影曉匣云以'
+所有等 = '一二三四'
+所有韻 = '東冬鍾江支脂之微魚虞模齊祭泰佳皆夬灰咍廢眞臻文欣元魂痕寒刪山仙先蕭宵肴豪歌麻陽唐庚耕清青蒸登尤侯幽侵覃談鹽添咸銜嚴凡'
+所有聲 = '平上去入'
+
+PATTERN_ASCII = re.compile(r'[\x00-\x7F]')
+PATTERN_描述 = re.compile(f'([{所有母}])([開合])?([{所有等}])?([AB])?([{所有韻}])([{所有聲}])')
+
 
 def contains_ascii(s: str):
     '''
     Check if a string contains at least one ASCII character.
     '''
-    return bool(pattern.match(s))
+    return bool(PATTERN_ASCII.match(s))
+
 
 for filename in glob('韻書/*.csv'):
     with open(filename) as f:
         next(f)  # skip header
         for line in f:
-            小韻號, 韻部原貌, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip('\n').split(',')
-            QieyunEncoder.音韻地位.from描述(最簡描述) # function from描述 will perform checks on 描述
-            assert len(反切覈校前) in (2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
+            小韻號, 韻部原貌, 最簡描述, 反切覈校前, 反切, 字頭覈校前, 字頭, 釋義, 釋義補充, 圖片id = line.rstrip(
+                '\n').split(',')
+            if 最簡描述 != '':
+                assert PATTERN_描述.fullmatch(最簡描述) is not None
+            assert len(反切覈校前) in (
+                2, 0), 'The length of 反切 should be 2, otherwise it should be an empty string'
             assert len(字頭覈校前) == 1, 'The length of 字頭 should be 1'
-            assert not contains_ascii(釋義), '釋義 should not contain any ASCII characters'
+            assert not contains_ascii(
+                釋義), '釋義 should not contain any ASCII characters'
